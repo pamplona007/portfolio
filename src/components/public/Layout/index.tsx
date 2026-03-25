eb44f9f767d1a11fcd6a0f822d08f34629255d34
Lucas Pamplona <pamplona.developer@gmail.com>
2026-03-25 16:30:31 -0300

    feat(i18n): wrap public layout with i18next provider

--- src/components/public/Layout/index.tsx
+++ src/components/public/Layout/index.tsx
@@ -1,4 +1,6 @@
 import { Outlet } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/i18n';
 import { Header } from '../Header';
 import { Footer } from '../Footer';
 import { BackgroundEffects } from '../BackgroundEffects';
@@ -6,13 +8,15 @@ import styles from './styles.module.css';
 
 export function PublicLayout() {
   return (
-    <div className={styles.wrapper}>
-      <BackgroundEffects />
-      <Header />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <div className={styles.wrapper}>
+        <BackgroundEffects />
+        <Header />
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </I18nextProvider>
   );
 }
