d399ebc311aa8ba656b61e8c00103db7eb801b72
Lucas Pamplona <pamplona.developer@gmail.com>
2026-03-25 17:31:51 -0300

    fix(i18n): move locale files to src/ so Vite can import them

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from '../public/locales/en/common.json';
-import pt from '../public/locales/pt/common.json';
+import en from './locales/en/common.json';
+import pt from './locales/pt/common.json';
 
 i18n
   .use(LanguageDetector)
